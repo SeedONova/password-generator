cda2c282e81c1dcc37728c95fb982889ecb1b03a
I Putu Bagus Widia Predana <78652589+SeedONova@users.noreply.github.com>
2024-04-23 10:12:34 +0700

    the project is completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,62 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'password-generator';
+  length = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  password = '';
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  onUseLetters(){
+    this.useLetters = !this.useLetters;
+  }
+
+  onUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onButtonClick(){
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.useLetters){
+      validChars += letters;
+    }
+    if(this.useNumbers){
+      validChars += numbers;
+    }
+    if(this.useSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i<this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
 }
